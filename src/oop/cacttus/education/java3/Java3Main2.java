22bc102fa4088ae92fb1e7ff5beb43d7a1a5ab62
Naim Sulejmani <naim.sulejmani@gmail.com>
2020-03-19 21:07:29 +0100

    online ushtrime 3

--- src/oop/cacttus/education/java3/Java3Main2.java
+++ src/oop/cacttus/education/java3/Java3Main2.java
@@ -1,8 +1,63 @@
 package oop.cacttus.education.java3;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Java3Main2 {
     public static void main(String[] args) {
-        Artikulli[] artikujt = new Artikulli[100];
-        
+        Artikulli[] vargu = new Artikulli[100];
+        Scanner reader = new Scanner(System.in);
+        int index = 0;
+
+        while (index < vargu.length) {
+            //vargu[index] = new Artikulli();
+            Artikulli artikulli = new Artikulli();
+            System.out.printf("Shkruaj ID te artikullit %d: %n", index + 1);
+            int id = reader.nextInt();
+            reader.nextLine();
+
+            if (id == -1) {
+                //nderprite while-in
+                break;
+            }
+
+            System.out.printf("Shkruaj emrin e artikullit %d: %n", index + 1);
+            String emri = reader.nextLine();
+
+            System.out.printf("Shkruaj daten (YYYY-MM-DD) e prodhimit te artikullit %d: %n", index + 1);
+            String dataProdhimitStr = reader.nextLine();
+            LocalDate dataProdhimit = LocalDate.parse(dataProdhimitStr);
+
+            artikulli.setId(id);
+            artikulli.setEmri(emri);
+            artikulli.setDataProdhimit(dataProdhimit);
+         /*
+         masi kam pengesa ne ze atehere po e plotesoj,sahere qe thirret new rezervohet vend i ri ne memorie
+         dhe ky artikull i ri me referencen ne memori i caktohet elemetitt te radhes ne varg te merr ate
+         reference, pastaj shkatrohet variabla dhe ne iteracionin e rradhes me komanden new rezervon perseri
+         memorie te re
+
+
+
+             */
+
+
+            vargu[index] = artikulli;
+
+            index++;
+        }
+
+        System.out.println("Total artikuj te zgjedhur = " + index);
+        System.out.println("Total artikuj te pazgjedhur  = " + (vargu.length - index));
+        //me vazhdu ketu pas while
+
+        int nr =0;
+        for(Artikulli a: vargu) {
+            if(a==null) break;
+            nr++;
+        }
+        System.out.println("Total artikuj te zgjedhur = " + nr);
+        System.out.println("Total artikuj te pazgjedhur  = " + (vargu.length - nr));
+
     }
 }
